ec774742ba60abb9eb456d7f287cd6d419de1f87
ubai1369 <ubai_1369@yahoo.com>
2012-11-07 14:52:03 +0700

    Penambahan Model

---
+++ model/PertanyaanModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtebakan.model;
+
+/**
+ *
+ * @author ubai
+ */
+public class PertanyaanModel {
+    private String jawaban;
+
+    public String getJawaban() {
+        return jawaban;
+    }
+
+    public void setJawaban(String jawaban) {
+        this.jawaban = jawaban;
+    }
+    
+    
+}
